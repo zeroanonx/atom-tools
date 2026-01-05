209c123a142b0fc0a65cd9f18b78aa7089c89a11
luwenbin <linhansong1008@gmail.com>
2026-01-05 11:38:34 +0800

    feat: optimize

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
